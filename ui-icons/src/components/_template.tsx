19dce53b16928d4ddaafa7df63c485717eccb311
Élysæ <101974839+ElysaSrc@users.noreply.github.com>
2023-12-22 17:12:37 +0100

    ui-icons: add design system icons

--- ui-icons/src/components/_template.tsx
+++ ui-icons/src/components/_template.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { IconData } from '../types/icon-data'
+import { default as sizes } from '../sizes'
 
 const iconData: IconData = {}
 
 //ReplaceWithTypes
 
 const IconReplaceName: React.FC<IconReplaceNameProps> = ({variant, size}) => {
+  const currentSize = sizes[size]
   if (!iconData[variant]) {
     throw new Error(`IconReplaceName: variant ${variant} not found.`)
   }
-  if (!iconData[variant][size]) {
-    throw new Error(`IconReplaceName: size ${size} not found for variant ${variant}.`)
+  if (!iconData[variant][currentSize]) {
+    throw new Error(`IconReplaceName: size ${currentSize} not found for variant ${variant}.`)
   }
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox={`0 0 ${size} ${size}`}
-      dangerouslySetInnerHTML={{ __html: iconData[variant][size] }}
+      width={currentSize}
+      height={currentSize}
+      viewBox={`0 0 ${currentSize} ${currentSize}`}
+      dangerouslySetInnerHTML={{ __html: iconData[variant][currentSize] }}
     />
   )
 }
