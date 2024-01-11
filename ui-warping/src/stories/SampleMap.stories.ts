2609f953d071bcb5d3324762779b447c22836e7f
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-01-11 17:17:23 +0100

    ui-warping: allows customizing warping
    Details:
    - Makes every setting from getWarping overridable
    - Adds inputs for all warping settings in algorithm showcase stories

--- ui-warping/src/stories/SampleMap.stories.ts
+++ ui-warping/src/stories/SampleMap.stories.ts
@@ -1,12 +1,20 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import SampleMap from './SampleMap.tsx';
+import { PATH_NAMES } from './helpers.ts';
 
 const meta: Meta<typeof SampleMap> = {
   component: SampleMap,
   title: 'WarpedMap component',
+  argTypes: {
+    path: {
+      options: PATH_NAMES,
+      control: { type: 'radio' },
+    },
+  },
 };
 
 export default meta;
+type Story = StoryObj<typeof SampleMap>;
 
-export const Primary = {};
+export const Primary: Story = {};
