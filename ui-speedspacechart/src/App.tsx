177ea8ab8ead191e661aeff244465ce1ab34625b
Yohh <durandyohan@zaclys.net>
2024-02-13 20:34:04 +0100

    ui-speedspacechart: init
    - create-react-app ui-speedspacechart
    - add taillwindcss using PostCSS
    - copy /ui-core/styles/main.css to replace /ui-speedspacechart/index.css
    - import main.css in index.tsx, remove import from App.tsx
    - edit tailwind.config.js
    - add IBMPlex fonts to /public
    - add storybook, clean /stories
    - clean unnecessary files

---
+++ ui-speedspacechart/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <span className="m-auto">coucou</span>
+    </div>
+  );
+}
+
+export default App;
