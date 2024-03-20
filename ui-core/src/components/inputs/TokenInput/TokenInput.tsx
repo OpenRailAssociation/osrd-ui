055badfb97fcbd3612265858e4366a8d8b9f460a
nncluzu <ngamenichaka@yahoo.fr>
2024-03-20 11:41:38 +0100

    ui-core: wrap token input

--- ui-core/src/components/inputs/TokenInput/TokenInput.tsx
+++ ui-core/src/components/inputs/TokenInput/TokenInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import cx from "classnames";
 import { X } from '@osrd-project/ui-icons';
 
@@ -15,20 +15,30 @@ const TokenInput : React.FC<TokenInputProps> = ({
     const [tokens, setTokens] = useState(initialTokens);
     const [newToken, setNewToken] = useState("");
     const [selectedToken, setSelectedToken] = useState<number|null>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleKeyDown = (e:React.KeyboardEvent<HTMLInputElement>) => {
+    const addToken = (e:React.KeyboardEvent<HTMLInputElement>) => {
         if(e.key === "Enter" && newToken.trim() !== ""){
             setTokens(oldTokens => [...oldTokens, newToken])
             setNewToken("")
         }
     }
 
+    const focusInput = (e:  React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (e.target === e.currentTarget) {
+            inputRef.current?.focus();
+        }
+      };
+
     return (
         <div className="token-input-wrapper">
             <label className="input-label">
                 {label}
             </label>
-            <div className="tokens-wrapper">
+            <div 
+                className="tokens-wrapper"
+                onClick={focusInput}
+            >
                 {tokens.map((token, index) => (
                     <div 
                         key={`${token}-${index}`}
@@ -48,8 +58,9 @@ const TokenInput : React.FC<TokenInputProps> = ({
                     type="text"
                     value={newToken}
                     onChange={e => setNewToken(e.target.value)}
-                    onKeyDown={handleKeyDown}
+                    onKeyDown={addToken}
                     className="token-input"
+                    ref={inputRef}
                 />
             </div>
         </div>
