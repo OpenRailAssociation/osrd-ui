ecf08d7b0efe80b55d99381880dcd2cc8aad5943
Mathieu Richard <mathieu.richard747@gmail.com>
2024-04-05 16:27:25 +0200

    ui-core: optimize rollup config for  storybook hotreload

--- ui-core/rollup.config.js
+++ ui-core/rollup.config.js
@@ -1,28 +1,32 @@
-import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
-import eslint from "@rollup/plugin-eslint";
-import postcss from "rollup-plugin-postcss";
+import eslint from '@rollup/plugin-eslint';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import livereload from 'rollup-plugin-livereload';
+import postcss from 'rollup-plugin-postcss';
 
-const formats = ["esm"];
+const formats = ['esm'];
 
 /** @type {import("rollup").RollupOptions} */
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: formats.map((format) => ({
     file: `dist/index.${format}.js`,
     format,
-    name: "osrdcore",
+    name: 'osrdcore',
     sourcemap: true,
   })),
   plugins: [
     eslint(),
     typescript(),
     postcss({
-      extract: "theme.css",
+      extract: 'theme.css',
       sourceMap: true,
       plugins: [],
     }),
     terser(),
+    livereload({
+      watch: 'dist',
+    }),
   ],
-  external: ["react"],
+  external: ['react'],
 };
