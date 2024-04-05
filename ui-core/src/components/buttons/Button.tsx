cf48ce50e6e718215336e812e4e2e245bd9756ca
Mathieu Richard <mathieu.richard747@gmail.com>
2024-04-05 15:38:36 +0200

    ui-core: button implementation

---
+++ ui-core/src/components/buttons/Button.tsx
@@ -0,0 +1,59 @@
+import cx from 'classnames';
+import React, { ReactNode } from 'react';
+import { Gear } from '@osrd-project/ui-icons';
+
+type ButtonVariant = 'Normal' | 'Cancel' | 'Quiet' | 'Destructive' | 'Primary';
+
+interface ButtonProps {
+  label: string;
+  variant?: ButtonVariant;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  leadingIcon?: ReactNode;
+  counter?: number;
+  size?: 'large' | 'medium' | 'small';
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  variant = 'Primary',
+  isLoading = false,
+  isDisabled = false,
+  leadingIcon = null,
+  counter = null,
+  size = 'large',
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (!isLoading && !isDisabled && onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <button
+      className={cx('button flex items-center', variant.toLowerCase(), size.toLowerCase(), {
+        loading: isLoading,
+      })}
+      onClick={handleClick}
+      disabled={isDisabled || isLoading}
+    >
+      {isLoading ? (
+        <>
+          <span className="icon">
+            <Gear variant="fill" size="lg" />
+          </span>
+        </>
+      ) : (
+        <>
+          {leadingIcon && <span className="leading-icon mr-2">{leadingIcon}</span>}
+          {label}
+          {counter !== null && <span className="counter ml-2">{counter}</span>}
+        </>
+      )}
+    </button>
+  );
+};
+
+export default Button;
---
+++ ui-core/src/stories/Button.stories.tsx
@@ -0,0 +1,97 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import '@osrd-project/ui-core/dist/theme.css';
+
+import Button from '../components/buttons/Button';
+
+import React from 'react';
+import { Archive, Bookmark, Cloud, Clock } from '@osrd-project/ui-icons';
+
+const icons = { archive: <Archive />, bookmark: <Bookmark />, cloud: <Cloud />, clock: <Clock /> };
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button',
+  tags: ['autodocs'],
+  argTypes: {
+    leadingIcon: {
+      options: Object.keys(icons), // An array of serializable values
+      mapping: icons, // Maps serializable option values to complex arg values
+      control: {
+        type: 'select', // Type 'select' is automatically inferred when 'options' is defined
+        labels: {
+          // 'labels' maps option values to string labels
+          Archive: 'Archive',
+          Bookmark: 'Bookmark',
+          Cloud: 'Cloud',
+          Clock: 'Clock',
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    label: 'Click me',
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    label: 'Loading',
+    isLoading: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled',
+    isDisabled: true,
+  },
+};
+
+export const Counter: Story = {
+  args: {
+    label: 'Counter',
+    counter: 5,
+  },
+};
+
+export const Quiet: Story = {
+  args: {
+    label: 'Quiet',
+    variant: 'Quiet',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    label: 'Destructive',
+    variant: 'Destructive',
+  },
+};
+
+export const Cancel: Story = {
+  args: {
+    label: 'Cancel',
+    variant: 'Cancel',
+  },
+};
+
+export const LeadingIcon: Story = {
+  args: {
+    label: 'Leading Icon',
+    leadingIcon: <Archive />,
+  },
+};
+
+export const LeadingIconCounter: Story = {
+  args: {
+    label: 'Leading Icon Counter',
+    leadingIcon: <Archive />,
+    counter: 5,
+  },
+};
