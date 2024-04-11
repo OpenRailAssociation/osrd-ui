48047091bd90ae1237533a4848b81aef09277529
Math_R_ <mathieu.richard747@gmail.com>
2024-04-11 19:26:35 +0200

    ui-core: button implementation (#32)

--- ui-core/src/index.ts
+++ ui-core/src/index.ts
@@ -1,5 +1,6 @@
 import './styles/main.css';
 
+export { default as Button } from './components/buttons/Button';
 export { default as Checkbox } from './components/inputs/checkbox';
 export { default as Input } from './components/inputs/Input';
 export { default as TokenInput } from './components/inputs/TokenInput';
