7822e4e6050aaf2377440ec1d4a4ef49c507cf33
Achraf.Mohyeddine <achraf.mohyeddine.ext@squirel.fr>
2024-04-11 23:22:22 +0200

    ui-core: implement radio buttons

--- ui-core/src/index.ts
+++ ui-core/src/index.ts
@@ -6,3 +6,4 @@ export { default as Input } from './components/inputs/Input';
 export { default as TokenInput } from './components/inputs/TokenInput';
 export { default as PasswordInput } from './components/inputs/PasswordInput';
 export { default as TextArea } from './components/inputs/TextArea';
+export { default as RadioButton } from './components/inputs/RadioGroup';
