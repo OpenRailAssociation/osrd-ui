26c16e571553096ead271b308949dcd49bf17145
nncluzu <ngamenichaka@yahoo.fr>
2024-04-25 17:45:40 +0200

    osrd-project: create tailwind preset at root
    osrd-project: create base rollup.config generator
    
    osrd-project: add base postcss.config

--- ui-core/postcss.config.cjs
+++ ui-core/postcss.config.cjs
@@ -1,12 +1,3 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-assets': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: { config: './tailwind.config.js' },
-    autoprefixer: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
-  },
-};
+const generateBasePostcssConfig = require('../postcss-base.config.cjs');
+
+module.exports = generateBasePostcssConfig();
