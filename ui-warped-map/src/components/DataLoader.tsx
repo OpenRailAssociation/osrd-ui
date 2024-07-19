d411625ce402dccb30e47a41b8fc9fff9c9c4b43
romainvalls <romain.valls95@gmail.com>
2024-07-19 11:28:39 +0200

    ui-warpedmap: fix useeffect warnings

--- ui-warped-map/src/components/DataLoader.tsx
+++ ui-warped-map/src/components/DataLoader.tsx
@@ -37,6 +37,7 @@ const DataLoader: FC<{
       if (log) console.time(TIME_LABEL);
       setState('render');
     }, 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mapRef, bbox]);
 
   useEffect(() => {
@@ -98,6 +99,7 @@ const DataLoader: FC<{
     }
 
     return undefined;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state]);
 
   return state !== 'loaded'
