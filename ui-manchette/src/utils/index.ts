ac8aa6b51c5d362fde49832f1f3bf0fc9a11eb88
Clara Ni <clara.ni@outlook.fr>
2024-08-29 14:42:58 +0200

    ui-manchette: fix space-time-chart proportional scale

--- ui-manchette/src/utils/index.ts
+++ ui-manchette/src/utils/index.ts
@@ -1,6 +1,7 @@
-import { FOOTER_HEIGHT } from '../components/consts';
+import { BASE_OP_HEIGHT, FOOTER_HEIGHT } from '../components/consts';
 
-export const getHeightWithoutFooter = (height: number) => height - FOOTER_HEIGHT;
+export const getHeightWithoutLastWaypoint = (height: number) =>
+  height - FOOTER_HEIGHT - BASE_OP_HEIGHT;
 
 export const positionMmToKm = (position: number) => Math.round((position / 1000000) * 10) / 10;
 
