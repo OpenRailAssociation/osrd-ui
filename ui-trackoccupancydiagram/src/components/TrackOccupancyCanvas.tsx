15dd538f86e9bf3d2bb620a7331566e33b4918bc
Yohh <durandyohan@zaclys.net>
2024-11-20 16:59:46 +0100

    ui-trackoccupancydiagram: prepare base html elements and samples
    Co-authored-by: Uriel-Sautron <uriel.sautron@gmail.com>
    Signed-off-by: Yohh <durandyohan@zaclys.net>

--- ui-trackoccupancydiagram/src/components/TrackOccupancyCanvas.tsx
+++ ui-trackoccupancydiagram/src/components/TrackOccupancyCanvas.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
-const TrackOccupancyCanvas = () => <div>TrackOccupancyCanvas</div>;
+import type { Store, TrackOccupancyCanvasProps } from './types';
+
+const TrackOccupancyCanvas = ({
+  zones,
+  selectedTrain,
+  timeOrigin,
+  timeScale,
+}: TrackOccupancyCanvasProps) => {
+  const store: Store = {
+    zones,
+    selectedTrain,
+    timeOrigin,
+    timeScale,
+    ratio: 0,
+    offsetX: 0,
+    clientX: 0,
+    clientY: 0,
+  };
+
+  console.warn(
+    'zones:',
+    zones,
+    'selectedTrain:',
+    selectedTrain,
+    'timeOrigin:',
+    timeOrigin,
+    'timeScale:',
+    timeScale,
+    'store:',
+    store
+  );
+
+  return <div id="trackOccupancyCanvas"></div>;
+};
 
 export default TrackOccupancyCanvas;
