946387e6e46eee1344e2644843738c80efa616f2
Yohh <durandyohan@zaclys.net>
2024-11-20 16:59:46 +0100

    ui-trackoccupancydiagram: create ui-trackoccupancydiagram story
    Co-authored-by: Uriel-Sautron <uriel.sautron@gmail.com>
    Signed-off-by: Yohh <durandyohan@zaclys.net>

---
+++ ui-trackoccupancydiagram/src/components/types.ts
@@ -0,0 +1,39 @@
+export type Track = {
+  id: string;
+  name: string;
+  line: string;
+};
+
+export type OccupancyZone = {
+  id: string;
+  trackId: string;
+  arrivalTrainName: string;
+  departureTrainName: string;
+  color: string;
+  originStation?: string;
+  destinationStation?: string;
+  arrivalTime: Date;
+  departureTime: Date;
+};
+
+export type TrackOccupancyCanvasProps = {
+  zones: OccupancyZone[];
+  selectedTrain: string | null;
+  timeOrigin: number; // in ms
+  timeScale: number; // in ms/px
+};
+
+export type TrackOccupancyManchetteProps = {
+  tracks: Track[];
+};
+
+export type Store = {
+  zones: OccupancyZone[];
+  selectedTrain: string | null;
+  timeOrigin: number; // in ms
+  timeScale: number; // in ms/px
+  ratio: number;
+  offsetX: number;
+  clientX: number;
+  clientY: number;
+};
