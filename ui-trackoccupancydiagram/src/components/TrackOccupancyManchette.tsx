e7e6f632941d9ae83d926f75714fc8d047f03bef
Yohh <durandyohan@zaclys.net>
2024-11-21 22:42:08 +0100

    ui-trackoccupancydiagram: display tracks in TrackOccupancyManchette.tsx
    Co-authored-by: Uriel-Sautron <uriel.sautron@gmail.com>
    Signed-off-by: Yohh <durandyohan@zaclys.net>

--- ui-trackoccupancydiagram/src/components/TrackOccupancyManchette.tsx
+++ ui-trackoccupancydiagram/src/components/TrackOccupancyManchette.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { type TrackOccupancyManchetteProps } from './types';
 
-const TrackOccupancyManchette = ({ tracks }: TrackOccupancyManchetteProps) => {
-  console.warn('tracks:', tracks);
-
-  return <div id="trackOccupancyManchette"></div>;
-};
+const TrackOccupancyManchette = ({ tracks }: TrackOccupancyManchetteProps) => (
+  <div id="track-occupancy-manchette">
+    {tracks.map((track) => (
+      <div className="track" key={track.id}>
+        <span className="track-line">{track.line}</span>
+        <div className="track-name">
+          <div className="track-name-detail">{track.name}</div>
+          <div className="track-name-rail" />
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
 export default TrackOccupancyManchette;
