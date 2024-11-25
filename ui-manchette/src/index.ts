e7d24162546a4db75147ab31ec5346c1f5308415
SharglutDev <p.filimon75@gmail.com>
2024-11-25 12:20:44 +0100

    ui-manchette: update some package exports
    - Add some type export needed when implementing the app
    - Remove some exports not needed outside of ui-manchette
    
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- ui-manchette/src/index.ts
+++ ui-manchette/src/index.ts
@@ -2,6 +2,7 @@ import '@osrd-project/ui-core/dist/theme.css';
 import './styles/main.css';
 import './components/consts';
 
-export { default as Waypoint } from './components/Waypoint';
-export { default as WaypointList } from './components/WaypointList';
-export { default as Manchette } from './components/Manchette';
+import Manchette from './components/Manchette';
+
+export default Manchette;
+export type { Waypoint, ProjectPathTrainResult } from './types';
