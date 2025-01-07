72af25fddd9ce3607757e2a65813b8d603b277ce
Valentin Chanas <anisometropie@gmail.com>
2025-01-07 15:23:49 +0100

    ui-manchette-with-spacetime: useful exports to reset zoom
    Signed-off-by: Valentin Chanas <anisometropie@gmail.com>

--- ui-manchette-with-spacetimechart/src/index.ts
+++ ui-manchette-with-spacetimechart/src/index.ts
@@ -1,3 +1,5 @@
 import './styles/main.css';
 
 export { default as useManchettesWithSpaceTimeChart } from './hooks/useManchetteWithSpaceTimeChart';
+export { DEFAULT_ZOOM_MS_PER_PX } from './consts';
+export { timeScaleToZoomValue } from './helpers';
