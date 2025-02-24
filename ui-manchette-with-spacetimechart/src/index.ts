142a1b56c9c67f2fae5f90f04ba62cbd56afcf58
Simon Ser <contact@emersion.fr>
2025-02-24 14:07:55 +0100

    ui-manchette-with-spacetimechart: add ManchetteWithSpaceTimeChart
    This is a component designed for simple use-cases. It's easier to
    use than the hook. Users can still decide to use the hook instead
    of this component, especially when they need more customizability.
    
    Other components can be passed in for the header and SpaceTimeChart,
    and additional properties can be passed to Manchette/SpaceTimeChart.
    
    This is a breaking API change: the hook is no longer the default
    export for ui-manchette-with-spacetimechart. Users need to replace:
    
        import useManchetteWithSpaceTimeChart from 'ui-manchette-with-spacetimechart';
    
    with:
    
        import { useManchetteWithSpaceTimeChart } from 'ui-manchette-with-spacetimechart';
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- ui-manchette-with-spacetimechart/src/index.ts
+++ ui-manchette-with-spacetimechart/src/index.ts
@@ -1,5 +1,6 @@
 import './styles/main.css';
 
-export { default as useManchettesWithSpaceTimeChart } from './hooks/useManchetteWithSpaceTimeChart';
+export { default as useManchetteWithSpaceTimeChart } from './hooks/useManchetteWithSpaceTimeChart';
+export { default as ManchetteWithSpaceTimeChart } from './components/ManchetteWithSpaceTimeChart';
 export { DEFAULT_ZOOM_MS_PER_PX } from './consts';
 export { timeScaleToZoomValue } from './helpers';
