4db8feab61f747204846e019a7cca6f98d2c4676
SharglutDev <p.filimon75@gmail.com>
2025-02-27 18:03:38 +0100

    ui-core: fix ui-core/input with small props
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- ui-core/src/components/inputs/Input.tsx
+++ ui-core/src/components/inputs/Input.tsx
@@ -92,9 +92,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           statusWithMessage?.tooltip || narrow ? 'before-status-message' : undefined
         }
         narrow={narrow}
-        className={cx('input-field-wrapper', inputFieldWrapperClassname, {
-          small,
-        })}
+        className={cx('input-field-wrapper', inputFieldWrapperClassname)}
         onCloseStatusMessage={onCloseStatusMessage}
       >
         {leadingContent && (
