0d44ec2e8261bc8ec384bfab929666f3d4686460
Simon Ser <contact@emersion.fr>
2025-04-04 15:23:47 +0200

    storybook: drop rules about stories inside ui-* packages
    There are no remaining stories inside ui-* packages.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -3,9 +3,6 @@ import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: [
-    // TODO: remove this when every stories are migrated to `@osrd-project/storybook/stories/`
-    '../../ui-*/src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../../ui-charts/src/**/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
