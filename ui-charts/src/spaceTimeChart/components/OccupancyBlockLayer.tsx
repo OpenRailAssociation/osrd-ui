d12dce712e53f25b1c62201c14563e2bbcaf5136
Theo Macron <theo.macron0315@gmail.com>
2025-04-07 16:42:56 +0200

    ui-charts: add striped color for blinking signals
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- ui-charts/src/spaceTimeChart/components/OccupancyBlockLayer.tsx
+++ ui-charts/src/spaceTimeChart/components/OccupancyBlockLayer.tsx
@@ -9,10 +9,29 @@ export type OccupancyBlock = {
   spaceStart: number;
   spaceEnd: number;
   color: string;
+  blinking?: boolean;
 };
 
 export type OccupancyBlockLayerProps = {
   occupancyBlocks: OccupancyBlock[];
+};
+
+const getFillStyle = (ctx: CanvasRenderingContext2D, color: string, isBlinking?: boolean) => {
+  if (isBlinking) {
+    const patternCanvas = document.createElement('canvas');
+    patternCanvas.width = 8;
+    patternCanvas.height = 8;
+    const pctx = patternCanvas.getContext('2d')!;
+    pctx.clearRect(0, 0, 8, 8);
+    pctx.strokeStyle = color;
+    pctx.lineWidth = 2;
+    pctx.beginPath();
+    pctx.moveTo(0, 8);
+    pctx.lineTo(8, 0);
+    pctx.stroke();
+    return ctx.createPattern(patternCanvas, 'repeat')!;
+  }
+  return color;
 };
 
 export const OccupancyBlockLayer = ({ occupancyBlocks }: OccupancyBlockLayerProps) => {
@@ -24,7 +43,7 @@ export const OccupancyBlockLayer = ({ occupancyBlocks }: OccupancyBlockLayerProp
         const width = getTimePixel(occupancyBlock.timeEnd) - x;
         const height = getSpacePixel(occupancyBlock.spaceEnd) - y;
 
-        ctx.fillStyle = occupancyBlock.color;
+        ctx.fillStyle = getFillStyle(ctx, occupancyBlock.color, occupancyBlock.blinking);
         ctx.fillRect(x, y, width, height);
       }
     },
