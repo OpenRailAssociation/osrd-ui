4f0a5285f9ac00744b961cd3f0c4c30766df5144
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-04-24 10:29:59 +0200

    ui-charts: improves splitting manchette and get
    Details:
    - Adds splitting waypoints in the operational points given to the GET
      from useManchetteWithSpaceTimeChart, and handles not rendering them in
      the GET code directly
    - Adds flatSteps in the SpaceTimeChartContext, as a Set<number>
    - Steps rendering path segments on pauses on flat steps, since we now
      assume they are rendered directly in the split section layer itself
    
    Signed-off-by: Alexis Jacomy <alexis.jacomy@gmail.com>

--- ui-charts/src/manchette/hooks/useManchetteWithSpaceTimeChart.tsx
+++ ui-charts/src/manchette/hooks/useManchetteWithSpaceTimeChart.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { Fragment, type ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { sortBy, clamp } from 'lodash';
 
@@ -432,15 +432,15 @@ const useManchetteWithSpaceTimeChart = ({
     splitPoints,
   ]);
 
-  const waypointsWithoutSplitPoints = useMemo(() => {
-    const splitPointPositions = new Set(splitPoints?.map((point) => point.position) || []);
-    const filteredWaypoints = selectWaypointsToDisplay(waypoints, {
-      height,
-      isProportional,
-      yZoom,
-    });
-    return filteredWaypoints.filter((waypoint) => !splitPointPositions.has(waypoint.position));
-  }, [splitPoints, waypoints, height, isProportional, yZoom]);
+  const waypointsToDisplay = useMemo(
+    () =>
+      selectWaypointsToDisplay(waypoints, {
+        height,
+        isProportional,
+        yZoom,
+      }),
+    [waypoints, height, isProportional, yZoom]
+  );
 
   const { manchetteContents, manchetteHeight } = useMemo(() => {
     const spaceScaleTree = spaceScalesToBinaryTree(spaceOrigin, spaceScales);
@@ -450,21 +450,24 @@ const useManchetteWithSpaceTimeChart = ({
       height - FOOTER_HEIGHT
     );
 
-    if (!splitPoints)
+    if (!splitPoints?.length)
       return {
         manchetteHeight: totalManchetteHeight,
-        manchetteContents: waypointsWithoutSplitPoints,
+        manchetteContents: waypointsToDisplay,
       };
 
     // Identify all manchette contents (split sections and waypoints):
+    const splitPointPositions = new Set(splitPoints.map((point) => point.position) || []);
     let allContents: (
       | { type: 'waypoint'; position: number; waypoint: Waypoint }
       | { type: 'splitSection'; position: number; split: SplitPoint }
-    )[] = waypointsWithoutSplitPoints.map((wp) => ({
-      type: 'waypoint',
-      waypoint: wp,
-      position: wp.position,
-    }));
+    )[] = waypointsToDisplay
+      .filter((wp) => !splitPointPositions.has(wp.position))
+      .map((wp) => ({
+        type: 'waypoint',
+        waypoint: wp,
+        position: wp.position,
+      }));
     allContents = allContents.concat(
       splitPoints.map((sp) => ({
         type: 'splitSection',
@@ -529,7 +532,7 @@ const useManchetteWithSpaceTimeChart = ({
       manchetteHeight: totalManchetteHeight,
       manchetteContents: finalContents,
     };
-  }, [spaceOrigin, spaceScales, splitPoints, waypointsWithoutSplitPoints, height]);
+  }, [spaceOrigin, spaceScales, splitPoints, waypointsToDisplay, height]);
 
   return useMemo<{
     manchetteProps: ManchetteProps;
@@ -557,11 +560,13 @@ const useManchetteWithSpaceTimeChart = ({
         yOffset,
       },
       spaceTimeChartProps: {
-        operationalPoints: waypointsWithoutSplitPoints.map((waypoint) => ({
+        operationalPoints: waypointsToDisplay.map((waypoint) => ({
           ...waypoint,
           importanceLevel: 1,
         })),
-        additionalChildren: splitPoints.map((sp) => sp.spaceTimeChartNode),
+        additionalChildren: splitPoints.map((sp, i) => (
+          <Fragment key={i}>{sp.spaceTimeChartNode}</Fragment>
+        )),
         timeScale: zoomValueToTimeScale(xZoom),
         xOffset,
         yOffset: -yOffset + WAYPOINTS_OFFSET,
@@ -670,7 +675,7 @@ const useManchetteWithSpaceTimeChart = ({
       yZoom,
       isProportional,
       yOffset,
-      waypointsWithoutSplitPoints,
+      waypointsToDisplay,
       xZoom,
       xOffset,
       timeOrigin,
